7a4b9082005b0c758c42b3edb9178f8c9064d14c
Andrea Gutierrez <gutierrezandrea012@gmail.com>
2022-06-07 17:18:37 -0500

    add data table

--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -10,14 +10,70 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        @yield('css')
         <!-- Styles -->
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
+        
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
+
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css"/>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.bootstrap5.min.css"/>
+        
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
         
-        @yield('js')
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        
+        <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+        
+        <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+        
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.bootstrap5.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+        <script>
+            $(document).ready(function () {
+                $('#post').DataTable({
+                    language:{
+                        "lengthMenu": "Mostrar _MENU_ registros",
+                        "zeroRecords": "No se encontraron resultados",
+                        "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                        "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                        "infoFiltered": "(filtrando de un total de _MAX_ registros)",
+                        "sSearch": "Buscar:",
+                        "oPaginate":{
+                            "sFirst": "Primero",
+                            "sLast": "Ultimo",
+                            "sNext": "Siguiente",
+                            "sPrevious":"Anterior"
+                        },
+                        "sProcessing":"Procesando...",
+                    },
+                    responsive: "true",
+                    dom: 'Bfrtilp',
+                    buttons:[
+                        {
+                            extend:'pdfHtml5',
+                            text: 'pdf',
+                            titleAttr:'Exportar a pdf',
+                            className:'btn btn-primary',
+                            exportOptions:{
+                                columns: [0,1,2,3,4,5]
+                            },
+                        }
+                    ]
+                });
+            });
+        </script>
+
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
         
     </head>
--- resources/views/dashboard/post/index.blade.php
+++ resources/views/dashboard/post/index.blade.php
@@ -1,68 +1,49 @@
 @extends('dashboard.layout')
 
-@section('css')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
-
-@endsection
-
 @section('content')
-<a class="btn btn-success my-3" href="{{route('post.create')}}">Crear</a>
-
-    <table class="table table-striped table-bordered shadow-lg mt-4" style="width: 100%" id="post">
-        <thead>
-            <tr>
-                <th>Titulo</th>
-                <th>Categoria</th>
-                <th>Posted</th>
-                <th>Contenido</th>
-                <th>Descripción</th>
-                <th>Imagen</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-           @foreach ($posts as $p)
-            <tr>
-                <td>{{$p->title}}</td>
-                <td>{{$p->category->title}}</td>
-                <td>{{$p->posted}}</td>
-                <td>{{$p->content}}</td>
-                <td>{{$p->description}}</td>
-                <td>
-                    <center>
-                        <img src="{{asset('image/otro/'.$p->image)}}" width="50%" alt="" srcset="">
-                    </center>
-                </td>
-                <td>
-                    <a class="btn mt-2 btn-primary" href="{{route('post.edit', $p)}}">Editar</a>
-                    <br>
-                    <a class="btn mt-2 btn-primary" href="{{route('post.show', $p)}}">Ver</a>
-                    <br>
-                    <form  action="{{ route("post.destroy", $p) }}" method="post">
-                        @method("DELETE")
-                        @csrf
-                        <button class="btn mt-2 btn-danger" type="submit">Eliminar</button>
-                    </form>
-                </td>
-            </tr>
-           @endforeach
-        </tbody>
-    </table>
-    {{$posts->links()}}
-    
-    @section('js')
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
-    
-    <script>
-        $(document).ready(function () {
-            $('#post').DataTable({
-                "lengthMenu" : [[5,10,50,-1],[5,10,50, "All"]]
-            });
-        });
-    </script>
-    @endsection
+<a class="btn btn-success my-3" style="width: 50%" href="{{route('post.create')}}">Crear</a>
+    <div class="table-responsive">
+        <table class="table table-bordered caption-top text-center" id="post">
+            <caption>List of users</caption>
+            <thead>
+                <tr>
+                    <th class="text-center">Titulo</th>
+                    <th class="text-center">Categoria</th>
+                    <th class="text-center">Posted</th>
+                    <th class="text-center">Contenido</th>
+                    <th class="text-center">Descripción</th>
+                    <th class="text-center">Imagen</th>
+                    <th class="text-center">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+               @foreach ($posts as $p)
+                <tr>
+                    <td>{{$p->title}}</td>
+                    <td>{{$p->category->title}}</td>
+                    <td>{{$p->posted}}</td>
+                    <td>{{$p->content}}</td>
+                    <td>{{$p->description}}</td>
+                    <td>
+                        <center>
+                            <img src="{{asset('image/otro/'.$p->image)}}" width="50%" alt="" srcset="">
+                        </center>
+                    </td>
+                    <td>
+                        <a class="btn mt-2 btn-primary" href="{{route('post.edit', $p)}}">Editar</a>
+                        <br>
+                        <a class="btn mt-2 btn-primary" href="{{route('post.show', $p)}}">Ver</a>
+                        <br>
+                        <form  action="{{ route("post.destroy", $p) }}" method="post">
+                            @method("DELETE")
+                            @csrf
+                            <button class="btn mt-2 btn-danger" type="submit">Eliminar</button>
+                        </form>
+                    </td>
+                </tr>
+               @endforeach
+            </tbody>
+        </table>
+    </div>
 
 @endsection
